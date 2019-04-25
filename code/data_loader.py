6871c824f6e9a863940445a6126c64e84101c739
Sajal Jain <jsajal1993@gmail.com>
2019-04-24 23:35:40 -0500

    Seperate tranformations for mask

--- code/data_loader.py
+++ code/data_loader.py
@@ -8,6 +8,7 @@ from torch.utils import data
 from utils import load_image
 import numpy as np
 
+import data_transform as transforms
 
 class MelanomaDataLoader(data.Dataset):
   """
@@ -18,13 +19,19 @@ class MelanomaDataLoader(data.Dataset):
                folder=None,
                num_classes=100,
                split="train",
-               transforms=None):
+               img_transforms=None):
     assert split in ["train", "val", "test"]
     # root folder, split
     self.root_folder = root_folder
     self.split = split
-    self.transforms = transforms
+    self.img_transforms = img_transforms
     self.n_classes = num_classes
+
+    #define mask tranforms
+    mask_transforms = []
+    mask_transforms.append(transforms.Scale((256,256)))
+    mask_transforms.append(transforms.ToTensor())
+    self.mask_transforms = transforms.Compose(mask_transforms)
 
     # load all labels
     if folder is None:
@@ -34,9 +41,9 @@ class MelanomaDataLoader(data.Dataset):
         'Label folder {:s} does not exist!'.format(folder))
     
     if split == "train":
-      start, end = 1, 2395 #1,1200; 1200,2400 #count = 1596
+      start, end = 1, 501 #1,1200; 1200,2400 #count = 1596
     elif split == "val":
-      start, end = 2395, 2595 #count = 500
+      start, end = 2495, 2595 #count = 500
     elif split == "test":
       start, end = 121, 151 #2095, 2595 #count = 500
 
@@ -73,9 +80,9 @@ class MelanomaDataLoader(data.Dataset):
     mask = np.ascontiguousarray(load_image(mask_filename, 0))
 
     # apply data augmentation
-    if self.transforms is not None:
-      img  = self.transforms(img)
-      mask  = self.transforms(mask)
+    if self.img_transforms is not None:
+      img  = self.img_transforms(img)
+      mask  = self.mask_transforms(mask)
       mask[mask > 0.5] = 1
       mask[mask <= 0.5] = 0
     return img, mask
